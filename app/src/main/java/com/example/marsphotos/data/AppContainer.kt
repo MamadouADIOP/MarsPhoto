f0427fac1b1b2105ec691791181645296cc64da1
CORPORATE\mdiop <mdiop@teksystems.com>
2024-09-25 00:04:58 -0400

    Add rest rule to change dispatcher

---
+++ app/src/main/java/com/example/marsphotos/data/AppContainer.kt
@@ -0,0 +1,38 @@
+package com.example.marsphotos.data
+
+import com.example.marsphotos.model.MarsPhoto
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+import retrofit2.http.GET
+
+interface AppContainer {
+    val marsPhotosRepository: MarsPhotosRepository
+}
+
+
+/**
+ * Retrofit service object for creating api calls
+ */
+interface MarsApiService {
+    @GET("photos")
+    suspend fun getPhotos(): List<MarsPhoto>
+}
+class DefaultAppContainer : AppContainer {
+
+    private val baseUrl =
+        "https://android-kotlin-fun-mars-server.appspot.com"
+
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
+        .baseUrl(baseUrl)
+        .build()
+
+    private val retrofitService: MarsApiService by lazy {
+        retrofit.create(MarsApiService::class.java)
+    }
+    override val marsPhotosRepository: MarsPhotosRepository by lazy {
+        NetworkMarsPhotosRepository(retrofitService)
+    }
+}
